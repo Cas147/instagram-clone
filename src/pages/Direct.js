529e1f1204351394fb45414185262403f2a18de5
Andres Castilla <andrescas1948@gmail.com>
2021-04-15 12:57:03 -0500

    finished Direct/inbox

--- src/pages/Direct.js
+++ src/pages/Direct.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 import AngleDown from '../images/512px-Angle_down_font_awesome.svg.png'
 import pencilIcon from '../images/pencil-icon.png'
+import sentIcon from '../images/sent-icon.png'
 
 import '../components/styles/Direct.css'
 
@@ -45,6 +46,14 @@ export default function Direct() {
                     </ul>
                 </div>
             </div>
+            <div className="text-message-container">
+                    <div className="text-img-container">
+                        <img className="text-img" src={sentIcon} alt="send icon"/>
+                    </div>
+                    <p>Your Messages</p>
+                    <h5>Send private photos and messages to a friend or group.</h5>
+                    <button>Send Message</button>
+                </div>
         </div>
     )
 }
